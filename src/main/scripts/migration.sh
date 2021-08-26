9386425f499f84dffbe3da790526e3005a4aded0
Zheng Chang <zhengchang@microsoft.com>
2021-08-26 18:06:59 +0800

    a

--- src/main/scripts/migration.sh
+++ src/main/scripts/migration.sh
@@ -247,7 +247,9 @@ EOF
 }
 
 function setupOracleBinary() {
-    runuser -l oracle -c '${JAVA_HOME}/bin/java -jar ${TARGET_BINARY_FILE_NAME} -targetOracleHomeLoc ${ORACLE_HOME} -invPtrLoc ${BASE_DIR}/oraInst.loc -javaHome ${JAVA_HOME}'
+    cmd="${JAVA_HOME}/bin/java -jar ${TARGET_BINARY_FILE_NAME} -targetOracleHomeLoc ${ORACLE_HOME} -invPtrLoc ${BASE_DIR}/oraInst.loc -javaHome ${JAVA_HOME}"
+    echo "cmd to run: $cmd"
+    runuser -l oracle -c '$cmd'
 }
 
 validateInputs
