e9a0ae09c877fc380f6eec77a2ae2b6a3edb9c2e
Zheng Chang <zhengchang@microsoft.com>
2021-08-30 15:46:33 +0800

    a

--- src/main/scripts/migration.sh
+++ src/main/scripts/migration.sh
@@ -25,8 +25,7 @@ export CHECK_URL="http://$wlsAdminURL/weblogic/ready"
 export startWebLogicScript="${DOMAIN_HOME}/startWebLogic.sh"
 export stopWebLogicScript="${DOMAIN_HOME}/bin/customStopWebLogic.sh"
 
-function echo_stderr ()
-{
+function echo_stderr() {
     echo "$@" >&2
 }
 
@@ -128,7 +127,6 @@ function cleanup() {
 
     rm -rf $JDK_HOME/jdk-11.0.9_linux-x64_bin.tar.gz
 
-    
     rm -f ${TMP_FILE_DIR}/azcopy.tar.gz
 
     rm -rf ${TMP_FILE_DIR}/azcopy
@@ -246,8 +244,7 @@ function downloadMigrationData() {
     echo "Download migration data end"
 }
 
-function create_oraInstloc()
-{
+function create_oraInstloc() {
     echo "creating Install Location Template..."
 
     cat <<EOF >$TMP_FILE_DIR/oraInst.loc.template
@@ -255,20 +252,18 @@ inventory_loc=[INSTALL_PATH]
 inst_group=[GROUP]
 EOF
 
-    sed 's@\[INSTALL_PATH\]@'"$ORACLE_INSTALL_PATH"'@' ${TMP_FILE_DIR}/oraInst.loc.template > ${TMP_FILE_DIR}/oraInst.loc
+    sed 's@\[INSTALL_PATH\]@'"$ORACLE_INSTALL_PATH"'@' ${TMP_FILE_DIR}/oraInst.loc.template >${TMP_FILE_DIR}/oraInst.loc
     sed -i 's@\[GROUP\]@'"$USER_GROUP"'@' ${TMP_FILE_DIR}/oraInst.loc
 }
 
-function setupOracleBinaryAndDomain() 
-{
+function setupOracleBinaryAndDomain() {
     cmd="${JAVA_HOME}/bin/java -jar ${TMP_FILE_DIR}/${TARGET_BINARY_FILE_NAME} -targetOracleHomeLoc ${ORACLE_HOME} -invPtrLoc ${TMP_FILE_DIR}/oraInst.loc -javaHome ${JAVA_HOME}"
     echo "cmd to run: $cmd"
     sudo runuser -l oracle -c "${cmd}"
     sudo unzip ${TMP_FILE_DIR}/${TARGET_DOMAIN_FILE_NAME} -d $(dirname "${DOMAIN_HOME}")
 }
 
-function createInputFile() 
-{
+function createInputFile() {
     echo "creating Install Location Template..."
 
     cat <<EOF >$TMP_FILE_DIR/inpute_file.template
@@ -279,32 +274,29 @@ function createInputFile()
 [source_host_name]=[target_host_name]
 EOF
 
-    sed 's@\[source_host_name\]@'"$SOURCE_HOST_NAME"'@' ${TMP_FILE_DIR}/inpute_file.template > ${TMP_FILE_DIR}/inpute_file
+    sed 's@\[source_host_name\]@'"$SOURCE_HOST_NAME"'@' ${TMP_FILE_DIR}/inpute_file.template >${TMP_FILE_DIR}/inpute_file
     sed -i 's@\[target_host_name\]@'"$TARGET_HOST_NAME"'@' ${TMP_FILE_DIR}/inpute_file
 }
 
-function crateWalletDirectory()
-{
+function crateWalletDirectory() {
     echo ${DOMAIN_ADMIN_PASSWORD} | sudo ${ORACLE_HOME}/oracle_common/common/bin/configWallet.sh -walletDir ${TMP_FILE_DIR} ${DOMAIN_ADMIN_USERNAME}
 }
 
-function runChangeHostCmd()
-{
+function runChangeHostCmd() {
     ${ORACLE_HOME}/oracle_common/bin/chghost.sh -chgHostInputFile ${TMP_FILE_DIR}/inpute_file \
-     -javaHome ${JAVA_HOME} \
-     -domainLoc ${DOMAIN_HOME} \
-     -domainAdminUserName ${DOMAIN_ADMIN_USERNAME} \
-     -walletDir ${TMP_FILE_DIR} \
-     -logDir ${TMP_FILE_DIR}
+        -javaHome ${JAVA_HOME} \
+        -domainLoc ${DOMAIN_HOME} \
+        -domainAdminUserName ${DOMAIN_ADMIN_USERNAME} \
+        -walletDir ${TMP_FILE_DIR} \
+        -logDir ${TMP_FILE_DIR}
 }
 
-function updateNetworkRules()
-{
+function updateNetworkRules() {
     # for Oracle Linux 7.3, 7.4, iptable is not running.
-    if [ -z `command -v firewall-cmd` ]; then
+    if [ -z $(command -v firewall-cmd) ]; then
         return 0
     fi
-    
+
     # for Oracle Linux 7.6, open weblogic ports
     echo "update network rules for admin server"
     sudo firewall-cmd --zone=public --add-port=$wlsAdminPort/tcp
@@ -315,10 +307,9 @@ function updateNetworkRules()
 }
 
 # Create adminserver as service
-function create_adminserver_service()
-{
-echo "Creating weblogic admin server service"
-cat <<EOF >/etc/systemd/system/wls_admin.service
+function create_adminserver_service() {
+    echo "Creating weblogic admin server service"
+    cat <<EOF >/etc/systemd/system/wls_admin.service
 [Unit]
 Description=WebLogic Adminserver service
 After=network-online.target
@@ -339,52 +330,47 @@ RestartSec=3
 [Install]
 WantedBy=multi-user.target
 EOF
-echo "Completed weblogic admin server service"
+    echo "Completed weblogic admin server service"
 }
 
-function admin_boot_setup()
-{
-echo "Creating admin server boot properties"
- #Create the boot.properties directory
- mkdir -p "$DOMAIN_HOME/servers/admin/security"
- echo "username=$DOMAIN_ADMIN_USERNAME" > "$DOMAIN_HOME/servers/admin/security/boot.properties"
- echo "password=$DOMAIN_ADMIN_PASSWORD" >> "$DOMAIN_HOME/servers/admin/security/boot.properties"
- sudo chown -R $username:$groupname $DOMAIN_HOME/servers
- echo "Completed admin server boot properties"
+function admin_boot_setup() {
+    echo "Creating admin server boot properties"
+    #Create the boot.properties directory
+    mkdir -p "$DOMAIN_HOME/servers/admin/security"
+    echo "username=$DOMAIN_ADMIN_USERNAME" >"$DOMAIN_HOME/servers/admin/security/boot.properties"
+    echo "password=$DOMAIN_ADMIN_PASSWORD" >>"$DOMAIN_HOME/servers/admin/security/boot.properties"
+    sudo chown -R $username:$groupname $DOMAIN_HOME/servers
+    echo "Completed admin server boot properties"
 }
 
-function enableAndStartAdminServerService()
-{
+function enableAndStartAdminServerService() {
     echo "Starting weblogic admin server as service"
     sudo systemctl enable wls_admin
     sudo systemctl daemon-reload
     sudo systemctl start wls_admin
 }
 
-function wait_for_admin()
-{
- #wait for admin to start
-count=1
-export CHECK_URL="http://$wlsAdminURL/weblogic/ready"
-status=`curl --insecure -ILs $CHECK_URL | tac | grep -m1 HTTP/1.1 | awk {'print $2'}`
-while [[ "$status" != "200" ]]
-do
-  echo "Waiting for admin server to start"
-  count=$((count+1))
-  if [ $count -le 30 ];
-  then
-      sleep 1m
-  else
-     echo "Error : Maximum attempts exceeded while starting admin server"
-     exit 1
-  fi
-  status=`curl --insecure -ILs $CHECK_URL | tac | grep -m1 HTTP/1.1 | awk {'print $2'}`
-  if [ "$status" == "200" ];
-  then
-     echo "Server started succesfully..."
-     break
-  fi
-done  
+function wait_for_admin() {
+    #wait for admin to start
+    count=1
+    export CHECK_URL="http://$wlsAdminURL/weblogic/ready"
+    adminStatus=$(curl --insecure -ILs $CHECK_URL | tac | grep -m1 HTTP/1.1 | awk {'print $2'})
+    while [[ "$adminStatus" != "200" ]]; do
+        echo "Waiting for admin server to start"
+        count=$((count + 1))
+        if [ $count -le 30 ]; then
+            sleep 1m
+        else
+            echo "Error : Maximum attempts exceeded while starting admin server"
+            exit 1
+        fi
+        adminStatus=$(curl --insecure -ILs $CHECK_URL | tac | grep -m1 HTTP/1.1 | awk {'print $2'})]
+        echo "adminStatus: $adminStatus"
+        if [ "$adminStatus" == "200" ]; then
+            echo "Server started succesfully..."
+            break
+        fi
+    done
 }
 
 validateInputs
